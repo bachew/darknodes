8bb78cd1fef01adff122e13d8b51e546314b7656
Chew Boon Aik <boon.aik.chew@f-secure.com>
2018-08-28 17:57:37 +0800

    add terraform-init -f flag, bump up to 0.0.6

--- src/inkbot/tasks.py
+++ src/inkbot/tasks.py
@@ -304,14 +304,15 @@ def restore(ctx, backup_file):
 
 
 @task
-def terraform_init(ctx):
+def terraform_init(ctx, force=False):
     '''
     Run 'terraform init' in darknode directories
     '''
     def init(dirname):
-        if not osp.exists(osp.join(dirname, '.terraform')) and glob(osp.join(dirname, '*.tf')):
-            with ctx.cd(dirname):
-                ctx.run(list2cmdline([darknode_bin('terraform'), 'init']))
+        if glob(osp.join(dirname, '*.tf')):
+            if force or not osp.exists(osp.join(dirname, '.terraform')):
+                with ctx.cd(dirname):
+                    ctx.run(list2cmdline([darknode_bin('terraform'), 'init']))
 
     init(darknode_dir)
 
